799388ea8db7b59908a958f1a6b64a0fff0ee250
Cássius Borges <C.T.Borges@outlook.com>
2024-01-30 19:12:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-// Obtém a referência do elemento com o ID "greeting"
 const greetingElement = document.getElementById("greeting");
 
-// Obtém a hora atual do sistema
 const currentHour = new Date().getHours();
 
-// Define a saudação com base na hora atual
-// if (currentHour >= 5 && currentHour < 12) {
-//   greetingElement.textContent = "Bom dia";
-// } else if (currentHour >= 12 && currentHour < 18) {
-//   greetingElement.textContent = "Boa tarde";
-// } else {
-//   greetingElement.textContent = "Boa noite";
-// }
-
-// Forma mais simples
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
     ? "Bom dia"
@@ -23,7 +11,6 @@ const greetingMessage =
 
 greetingElement.textContent = greetingMessage;
 
-// GRID INTELIGENTE
 const container = document.querySelector(".offer__list-item");
 
 const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
